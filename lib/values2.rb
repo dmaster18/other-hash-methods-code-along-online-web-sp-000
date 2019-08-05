d4383e5f2e60c9244d261e34dfdf8645998a2eff
David Strauch <dmaster18@gmail.com>
2019-08-05 19:09:32 +0000

    Automatically backed up by Learn

--- lib/values2.rb
+++ lib/values2.rb
@@ -3,7 +3,7 @@ require 'pry'
 #This is the groceries hash we'll be passing in to the get_the_values method
  groceries = {
   :dairy => {
-    milk: ["soy", "almond", "cow", "goat"], 
+    :milk => ["soy", "almond", "cow", "goat"], 
     non-milk: ["yogurt", "cheese"]
   },
   vegetable: ["carrots", "broccoli", "cucumbers"],
